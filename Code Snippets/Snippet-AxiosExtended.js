d7ac0403b5e6467567c01b97f90c41b7d43a0d80
Kelvin Wijkniet <31469915+KWijkniet@users.noreply.github.com>
2022-06-27 15:55:46 +0200

    Update Snippet-AxiosExtended.js

--- Code Snippets/Snippet-AxiosExtended.js
+++ Code Snippets/Snippet-AxiosExtended.js
@@ -1,23 +1,32 @@
 axiosRequest: function (type, url, data, optional = {}){
+    //get loader component
     var loader = this.$root.loader;
+    
+    //create loader item
     var loaderItem = null;
     if(loader){
         loaderItem = loader.add(url);
     }
 
+    //execute a new axios request based on given variables
     return new Promise((res, rej) => {
         axios[type](url, data, optional)
         .then((response) => {
+            //remove loader item
             if(loader){
                 loader.remove(loaderItem);
             }
+
+            //return the response as a success
             res(response);
         })
         .catch((err) =>{
-            //check if user session is valid
+            //remove loader item
             if(loader){
                 loader.remove(loaderItem);
             }
+
+            //return the response as an error
             rej(err);
         })
     });
